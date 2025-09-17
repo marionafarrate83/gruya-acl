9c52b152163b9d8f91829c41cdccff8aae8b1635
mario perez n <mario.nafarrate@gmail.com>
2025-09-17 13:40:51 -0600

    set1

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 
 // Conectar a MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
-  //useNewUrlParser: true,
-  //useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 .then(() => console.log('Conectado a MongoDB'))
 .catch(err => console.log('Error conectando a MongoDB:', err));
